4ef7d751e86db68c6bf4077102fc4caf6a06ccad
obkin <kapellwork@gmail.com>
2025-01-12 23:24:51 +0200

    added: basic styles

--- src/components/ModelViewer/ModelViewer.jsx
+++ src/components/ModelViewer/ModelViewer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ModelViewer.css';
 
 const ModelViewer = ({ modelProps }) => {
   return (
--- src/components/SpecificationTable/SpecificationTable.jsx
+++ src/components/SpecificationTable/SpecificationTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SpecificationTable.css';
 
 const SpecificationTable = ({ specifications }) => {
   return (
